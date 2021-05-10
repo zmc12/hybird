fc6077669cb5646d22692c6626225b2b827a327f
tom_glb <goodMoring_glb@qq.com>
2021-05-10 20:20:26 +0800

    接口更新

--- src/main/java/com/jsut/hybird/config/SwaggerConfiguration.java
+++ src/main/java/com/jsut/hybird/config/SwaggerConfiguration.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.Api;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -20,7 +22,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 @ConditionalOnProperty(prefix = "mconfig", name = "swagger-ui-open", havingValue = "true")
-public class SwaggerConfiguration {
+public class SwaggerConfiguration implements WebMvcConfigurer {
 
     /**
      * 创建API应用
@@ -48,5 +50,15 @@ public class SwaggerConfiguration {
                 .version("1.0")
                 .build();
     }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
 
 }
